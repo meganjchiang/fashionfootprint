eb754d6aaf7d9740514d316d50042359ae1456f1
jasminepw <115040328+jasminepw@users.noreply.github.com>
2024-05-26 18:19:07 -0700

    editing to include site

--- code/chrome_extension/aritzia_scrape.py
+++ code/chrome_extension/aritzia_scrape.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
@@ -19,6 +20,11 @@ def scrape_materials(url):
             
         # content
         content = BeautifulSoup(review.text, 'html.parser')
+
+        # site name
+        meta_tag = content.find('meta', property='og:site_name')
+        site = meta_tag['content'][:-4]
+        # print(site)
 
         # extract item name
         item_name_element = content.find('h1', class_='js-product-detail__product-name')
@@ -51,7 +57,7 @@ def scrape_materials(url):
             # return first set of materials (for the clothing body) that add up to 100%
             if total == 100:
                 # Create a new dictionary with 'item' key listed first
-                final_dict = {'item': item_name}
+                final_dict = {'item': item_name, 'site': site}
                 final_dict.update(body_materials)
                 return final_dict
             
@@ -60,5 +66,10 @@ def scrape_materials(url):
     except Exception as e:
         return e
 
-print(scrape_materials('https://www.aritzia.com/us/en/product/olive-midi-pleated-skirt/112453.html?dwvar_112453_color=31493'))
-print(scrape_materials('https://www.aritzia.com/us/en/product/sinch-smooth-willow-t-shirt/105194.html?dwvar_105194_color=1274'))
+# print(scrape_materials('https://www.aritzia.com/us/en/product/olive-midi-pleated-skirt/112453.html?dwvar_112453_color=31493'))
+# print(scrape_materials('https://www.aritzia.com/us/en/product/sinch-smooth-willow-t-shirt/105194.html?dwvar_105194_color=1274'))
+
+# print(scrape_materials('https://www.aritzia.com/us/en/product/homestretch%E2%84%A2-crew-waist-raglan-t-shirt/117914005.html?dwvar_117914_color=32610'))
+# print(scrape_materials('https://www.aritzia.com/us/en/product/hold-it%E2%84%A2-new-bergman-tank/116146.html'))
+# print(scrape_materials('https://www.aritzia.com/us/en/product/sinch-baby-rib-self-tank/116805002.html?Quantity=1&uuid=ecc87ae5e08f6fc157a3a622a5'))
+# print(scrape_materials('https://www.aritzia.com/us/en/product/sinch-smooth-willow-t-shirt/105194.html?dwvar_105194_color=1274'))
