b0342f75c38afd9c63101d3eac98c2cb324995f7
jasminepw <115040328+jasminepw@users.noreply.github.com>
2024-05-26 18:06:35 -0700

    scraping aritzia chrome

---
+++ code/chrome_extension/aritzia_scrape.py
@@ -0,0 +1,64 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from bs4 import BeautifulSoup
+import re
+
+# Create session
+session = requests.Session()
+# Retry five times in case of exception
+retry = Retry(connect=5, backoff_factor=0.5)
+# Apply delays between attempts
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('https://', adapter)
+
+def scrape_materials(url):
+    try:
+        # search for brand review
+        review = session.get(url)
+            
+        # content
+        content = BeautifulSoup(review.text, 'html.parser')
+
+        # extract item name
+        item_name_element = content.find('h1', class_='js-product-detail__product-name')
+        item_name = item_name_element.text.strip().title() if item_name_element else "Unknown"
+
+        # extract materials 
+        norm_text = str(content).lower()
+
+        # search for percentage plus material
+        result = re.findall('[\d.]+%\s+(?!off)+[^\t\n\r\d~`!@#$%^&*()_\-+=[\]\{\}[\]|\\:;"\'<>,.?/]+', norm_text)
+
+        all_materials = {}
+        for item in result:
+            percent, material = re.split('%\s+', item)
+
+            # check if percentage is an integer or float
+            if '.' in percent:
+                percent = float(percent) 
+            else:
+                percent = int(percent) 
+            all_materials[material] = percent
+
+        # check if percentages add up to 100%
+        total = 0
+        body_materials = {}
+        for key, value in all_materials.items():
+            total += value
+            body_materials[key] = value
+
+            # return first set of materials (for the clothing body) that add up to 100%
+            if total == 100:
+                # Create a new dictionary with 'item' key listed first
+                final_dict = {'item': item_name}
+                final_dict.update(body_materials)
+                return final_dict
+            
+        return "Material percentages do not add up to 100%"
+                
+    except Exception as e:
+        return e
+
+print(scrape_materials('https://www.aritzia.com/us/en/product/olive-midi-pleated-skirt/112453.html?dwvar_112453_color=31493'))
+print(scrape_materials('https://www.aritzia.com/us/en/product/sinch-smooth-willow-t-shirt/105194.html?dwvar_105194_color=1274'))
